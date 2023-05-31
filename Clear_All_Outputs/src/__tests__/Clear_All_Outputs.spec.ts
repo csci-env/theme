277f25cf701ee045d60e4e42f2342618b622427b
shianli.chen@ontariotechu.net <shianli.chen@ontariotechu.net>
2023-05-31 20:27:56 +0000

    Added Clear All Ouputs and Run All Cells Toolbar buttons

---
+++ Clear_All_Outputs/src/__tests__/Clear_All_Outputs.spec.ts
@@ -0,0 +1,9 @@
+/**
+ * Example of [Jest](https://jestjs.io/docs/getting-started) unit tests
+ */
+
+describe('Clear_All_Outputs', () => {
+  it('should be tested', () => {
+    expect(1 + 1).toEqual(2);
+  });
+});
---
+++ Run_All_Toolbar_Choice/src/index.ts
@@ -0,0 +1,73 @@
+import { IDisposable, DisposableDelegate } from '@lumino/disposable';
+
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin,
+} from '@jupyterlab/application';
+
+import { ToolbarButton } from '@jupyterlab/apputils';
+
+import { DocumentRegistry } from '@jupyterlab/docregistry';
+
+import {
+  NotebookActions,
+  NotebookPanel,
+  INotebookModel,
+} from '@jupyterlab/notebook';
+
+/**
+ * The plugin registration information.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  activate,
+  id: 'run-all-cells-button',
+  autoStart: true,
+};
+
+/**
+ * A notebook widget extension that adds a button to the toolbar.
+ */
+export class ButtonExtension
+  implements DocumentRegistry.IWidgetExtension<NotebookPanel, INotebookModel>
+{
+  /**
+   * Create a new extension for the notebook panel widget.
+   *
+   * @param panel Notebook panel
+   * @param context Notebook context
+   * @returns Disposable on the added button
+   */
+  createNew(
+    panel: NotebookPanel,
+    context: DocumentRegistry.IContext<INotebookModel>
+  ): IDisposable {
+    const runAllCells = () => {
+      NotebookActions.runAll(panel.content, panel.context.sessionContext);
+    };
+    const button = new ToolbarButton({
+      className: 'run-all-button',
+      label: 'Run All Cells',
+      onClick: runAllCells,
+      tooltip: 'Run All Cells',
+    });
+
+    panel.toolbar.insertItem(10, 'runAllCells', button);
+    return new DisposableDelegate(() => {
+      button.dispose();
+    });
+  }
+}
+
+/**
+ * Activate the extension.
+ *
+ * @param app Main application object
+ */
+function activate(app: JupyterFrontEnd): void {
+  app.docRegistry.addWidgetExtension('Notebook', new ButtonExtension());
+}
+
+/**
+ * Export the plugin as default.
+ */
+export default plugin;
